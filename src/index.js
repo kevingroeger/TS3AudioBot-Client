d3be3aa45feea6f40f2a139edfd8624f83152a3a
Kevin Gröger <k.groeger@headtrip.eu>
2020-04-25 13:01:22 +0200

    indexjs clean up

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
-/*
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-) */
 
 ReactDOM.render(
   <Provider store={store}>
